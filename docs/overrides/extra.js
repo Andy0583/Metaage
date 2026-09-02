6862b50e19df594d6d3fc28e71771000c87efbea
Andy Hsu <andy0583@gmail.com>
2026-09-02 19:23:10 +0800

    Delete docs/overrides/extra.js

--- docs/overrides/extra.js
+++
@@ -1,5 +0,0 @@
-var script = document.createElement('script');
-script.setAttribute('data-goatcounter', 'https://andy0583.goatcounter.com/count');
-script.async = true;
-script.src = '//gc.zgo.at/count.js';
-document.head.appendChild(script);
