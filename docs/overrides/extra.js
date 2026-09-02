0c9804df22ea125f1e36e4ff9f9649f0830b3d20
Andy Hsu <andy0583@gmail.com>
2026-09-02 19:12:32 +0800

    Create extra.js

---
+++ docs/overrides/extra.js
@@ -0,0 +1,5 @@
+var script = document.createElement('script');
+script.setAttribute('data-goatcounter', 'https://andy0583.goatcounter.com/count');
+script.async = true;
+script.src = '//gc.zgo.at/count.js';
+document.head.appendChild(script);
