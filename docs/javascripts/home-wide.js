596ff700b7415390b7ba604323b88dd3bb4d50a3
Andy Hsu <andy0583@gmail.com>
2026-09-05 00:59:44 +0800

    Delete docs/javascripts/home-wide.js

--- docs/javascripts/home-wide.js
+++
@@ -1,8 +0,0 @@
-document$.subscribe(function() {
-  var path = location.pathname;
-  if (path === "/metaage/" || path.endsWith("/metaage/index.html")) {
-    document.body.classList.add("home-wide");
-  } else {
-    document.body.classList.remove("home-wide");
-  }
-});
